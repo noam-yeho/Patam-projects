6452f4634b6b46c12ceeff3a7d4fb2020204af91
noamnoam-yeho <noamyeho@gmail.com>
2026-01-23 10:25:16 +0200

    finish assignment number 1

--- משימה - 1/test/Message.java
+++ משימה - 1/test/Message.java
@@ -22,22 +22,10 @@ public class Message {
     }
 
     public Message(byte[] data) {
-        this.data = data;
-        this.asText = new String(data);
-        double tempDouble;
-        try {
-            tempDouble = Double.parseDouble(this.asText);
-        } catch (NumberFormatException e) {
-            tempDouble = Double.NaN;
-        }
-        this.asDouble = tempDouble;
-        this.date = new Date();
+        this(new String(data));
     }
 
     public Message(double asDouble) {
-        this.asDouble = asDouble;
-        this.asText = Double.toString(asDouble);
-        this.data = this.asText.getBytes();
-        this.date = new Date();
+        this(Double.toString(asDouble));
     }
 }
